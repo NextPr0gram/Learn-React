2c7cf76d23820f491ef199f77c68d3be04293b89
NextProgram <anafkarim@proton.me>
2023-12-15 16:56:31 +0000

    style: change tabsize from 2 to 4 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
